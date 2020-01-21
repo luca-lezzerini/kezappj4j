fe3ee3f7d74ccc07eb5a79850ef19e3c47605f45
Luca Lezzerini <luca@lezzerini.it>
2020-01-21 12:22:45 +0100

    Prima versione, commit iniziale, solo Web, no DB

---
+++ src/test/java/com/example/kezapp/KezappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kezapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KezappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
