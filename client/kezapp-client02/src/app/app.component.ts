682609574b2930deeeaad1a2162c2560152ec3d3
emanuele-cancelli <emanuele.cancelli1@gmail.com>
2020-01-25 15:02:53 +0100

    Aggiornato Client02 con registrazione, inviaTutti, css

--- client/kezapp-client02/src/app/app.component.ts
+++ client/kezapp-client02/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { RichiediRegistrazioneDto } from './richiediRegistrazioneDto';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { RegistrazioneDto } from './registrazioneDto';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,23 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  nickName: string;
+  private nickName: string;
   messaggio: string;
-  righe: string[] = [];
-  sessione: string;
+  righe: string[][] = [['Row1', 'Text', 'Text', 'Text'], ['Row2', 'Text', 'Text', 'Text'],['Row3', 'Text', 'Text', 'Text']];
+  sessione: string = 'ciao';
   rrd: RichiediRegistrazioneDto;
-  constructor(private http: HttpClient){
+  registrazioneDto: RegistrazioneDto;
+  constructor(private http: HttpClient) {
 
   }
   registrazione() {
     this.rrd.nickname = this.nickName;
-    let obs : Observable<RichiediRegistrazioneDto> = this.http.get<RichiediRegistrazioneDto>('http://localhost:8080/KezappController02');
+    let obs: Observable<RegistrazioneDto> =
+      this.http.get<RegistrazioneDto>('http://localhost:8080/KezappController02/${this.rrd.nickname}');
+    obs.subscribe(x => this.registrazioneDto = x);
+  }
+  inviaTutti() {
+    let obs: Observable<any> = this.http.get('http://localhost:8080/KezappController02');
     obs.subscribe();
   }
 }
