80400fabf76ea651a2941c8372ed46ea0b01b88f
Andrea D'Ottavio <andreadottavio70@gmail.com>
2020-01-28 14:15:23 +0100

    controller 07

--- server/src/main/java/com/example/kezapp/controller/KezappController07.java
+++ server/src/main/java/com/example/kezapp/controller/KezappController07.java
@@ -1,7 +1,19 @@
 
 package com.example.kezapp.controller;
 
+import com.example.kezapp.model.RegistrazioneDto07;
+import com.example.kezapp.model.RichiediRegistrazioneDto07;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class KezappController07 {
-    
+    @RequestMapping(value = "/registrazione07")
+    public RegistrazioneDto07 registrazione(){
+        System.out.println("siamo in registrazione :)");
+        RegistrazioneDto07 rx = new RegistrazioneDto07();
+        rx.setSessione("123stella");
+        return rx;
+    }
 }
