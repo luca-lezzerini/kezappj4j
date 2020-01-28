59ba7080ed673a2501a642f8ea8ecb6de1b2ca21
Manuel Massa <manueldeho@gmail.com>
2020-01-28 14:37:00 +0100

    Aggiornamento controller/KezappController10.java

--- server/src/main/java/com/example/kezapp/controller/KezappController10.java
+++ server/src/main/java/com/example/kezapp/controller/KezappController10.java
@@ -16,4 +16,30 @@ public class KezappController10 {
         rx.setSessione("123stella!");
         return rx;
     }
+    
+    @RequestMapping(value = "/inviaatutti10")
+    public RegistrazioneDto10 inviaATutti(){
+        System.out.println("Siamo in Invia a tutti!");
+        RegistrazioneDto10 rx = new RegistrazioneDto10();
+        rx.setSessione("Sessione Invia a tutti");
+        return rx;
+    }
+    
+    @RequestMapping(value = "/inviaauno10")
+    public RegistrazioneDto10 inviaAUno(){
+        System.out.println("Siamo in Invia A Uno!");
+        RegistrazioneDto10 rx = new RegistrazioneDto10();
+        rx.setSessione("Sessione Invia a uno!");
+        return rx;
+    }
+   
+    @RequestMapping(value = "/aggiorna10")
+    public RegistrazioneDto10 aggiorna(){
+        System.out.println("Siamo in aggiorna!");
+        RegistrazioneDto10 rx = new RegistrazioneDto10();
+        rx.setSessione("Sessione Aggiorna!");
+        return rx;
+    }
+    
+    
 }
