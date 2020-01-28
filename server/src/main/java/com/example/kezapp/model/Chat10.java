7e7984a9ff2f1effbef1294e3adc7ee455ee0c89
Manuel Massa <manueldeho@gmail.com>
2020-01-28 12:12:00 +0100

    Creazione classi in server.

--- server/src/main/java/com/example/kezapp/model/Chat10.java
+++ server/src/main/java/com/example/kezapp/model/Chat10.java
@@ -6,4 +6,43 @@ public class Chat10 {
     private String nickname;
     private String sessione;
 
+    public Chat10() {
+    }
+
+    public Chat10(Long id, String nickname, String sessione) {
+        this.id = id;
+        this.nickname = nickname;
+        this.sessione = sessione;
+    }
+    
+    
+
+    public Long getId() {
+        if(id == null){
+            id = 0L;
+        }
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getSessione() {
+        return sessione;
+    }
+
+    public void setSessione(String sessione) {
+        this.sessione = sessione;
+    }
+    
+    
 }
