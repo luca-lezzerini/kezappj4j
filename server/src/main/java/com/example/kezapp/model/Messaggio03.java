e809a685fa2e808a3dece1ac27df39a3ce47280b
Leonardocaracci <Leonardossl2000@gmail.com>
2020-01-28 12:08:22 +0100

    creato chat e messaggio

---
+++ server/src/main/java/com/example/kezapp/model/Messaggio03.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.kezapp.model;
+
+/**
+ *
+ * @author utente
+ */
+public class Messaggio03 {
+    
+    private Long id;
+    private String testo;
+    private String aliasDestinatario;
+    private String aliasMittente;
+
+    public Long getId() {
+        if(id==null){
+            id=0L;
+            
+        }
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTesto() {
+        return testo;
+    }
+
+    public void setTesto(String testo) {
+        this.testo = testo;
+    }
+
+    public String getAliasDestinatario() {
+        return aliasDestinatario;
+    }
+
+    public void setAliasDestinatario(String aliasDestinatario) {
+        this.aliasDestinatario = aliasDestinatario;
+    }
+
+    public String getAliasMittente() {
+        return aliasMittente;
+    }
+
+    public void setAliasMittente(String aliasMittente) {
+        this.aliasMittente = aliasMittente;
+    }
+    
+}
