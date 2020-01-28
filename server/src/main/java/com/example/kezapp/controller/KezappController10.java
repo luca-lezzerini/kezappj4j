3c0f4453af9f84f7bfb82293f521e64da0cae1ee
Manuel Massa <manueldeho@gmail.com>
2020-01-28 14:20:11 +0100

    Aggiornamento controller/KezappController10.java

--- server/src/main/java/com/example/kezapp/controller/KezappController10.java
+++ server/src/main/java/com/example/kezapp/controller/KezappController10.java
@@ -1,5 +1,19 @@
 package com.example.kezapp.controller;
 
+import com.example.kezapp.model.RegistrazioneDto10;
+import com.example.kezapp.model.RichiediRegistrazioneDto10;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class KezappController10 {
     
+    @RequestMapping(value = "/registrazione10")
+    public RegistrazioneDto10 registrazione(){
+        System.out.println("Siamo in registrazione!");
+        RegistrazioneDto10 rx = new RegistrazioneDto10();
+        rx.setSessione("123stella!");
+        return rx;
+    }
 }
