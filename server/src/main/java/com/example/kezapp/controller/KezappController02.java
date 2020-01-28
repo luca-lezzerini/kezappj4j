60a119547b44565f754205d54ff763e26bcdeaa4
emanuele-cancelli <emanuele.cancelli1@gmail.com>
2020-01-28 14:33:12 +0100

    registrazione controller02

--- server/src/main/java/com/example/kezapp/controller/KezappController02.java
+++ server/src/main/java/com/example/kezapp/controller/KezappController02.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.example.kezapp.controller;
 
-/**
- *
- * @author utente
- */
+import com.example.kezapp.model.RegistrazioneDto02;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class KezappController02 {
-    
+
+    @RequestMapping(value = "/registrazione02")
+    public RegistrazioneDto02 registrazione() {
+        System.out.println("Siamo in registrazione!");
+        RegistrazioneDto02 rx = new RegistrazioneDto02();
+        rx.setSessione("123stella!");
+        return rx;
+    }
+
 }
