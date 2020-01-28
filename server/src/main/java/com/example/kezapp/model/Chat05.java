6270cf2f4121d1ca44970d251f2355c9aac39e63
Massimo Coruzzo <coruxrome@gmail.com>
2020-01-28 12:16:51 +0100

    Create classi

--- server/src/main/java/com/example/kezapp/model/Chat05.java
+++ server/src/main/java/com/example/kezapp/model/Chat05.java
@@ -10,5 +10,45 @@ package com.example.kezapp.model;
  * @author utente
  */
 public class Chat05 {
+    private Long id;
+    private String sessione;
+    private String nickname;
+
+    public Long getId() {
+        if (id == null){
+            id = 0L;
+        }
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSessione() {
+        return sessione;
+    }
+
+    public void setSessione(String sessione) {
+        this.sessione = sessione;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Chat05(Long id, String sessione, String nickname) {
+        this.id = id;
+        this.sessione = sessione;
+        this.nickname = nickname;
+    }
+
+    public Chat05() {
+    }
+    
     
 }
