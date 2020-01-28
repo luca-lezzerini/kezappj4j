116b37224b8dc148f8959a8b7e9956744afcde84
Andrea D'Ottavio <andreadottavio70@gmail.com>
2020-01-28 14:35:04 +0100

    controller 07

--- server/src/main/java/com/example/kezapp/controller/KezappController07.java
+++ server/src/main/java/com/example/kezapp/controller/KezappController07.java
@@ -8,12 +8,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class KezappController07 {
+public class KezappController07{
     @RequestMapping(value = "/registrazione07")
     public RegistrazioneDto07 registrazione(){
         System.out.println("siamo in registrazione :)");
         RegistrazioneDto07 rx = new RegistrazioneDto07();
         rx.setSessione("123stella");
+        return rx;
+    }
+    @RequestMapping(value = "/inviaTutti07")
+    public RegistrazioneDto07 inviaTutti(){
+        RegistrazioneDto07 rx = new RegistrazioneDto07();
+        rx.setSessione("invia a tutti");
+        return rx;
+    }
+    @RequestMapping(value = "/inviaUno07")
+    public RegistrazioneDto07 inviaUno(){
+        RegistrazioneDto07 rx = new RegistrazioneDto07();
+        rx.setSessione("invia a uno");
+        return rx;
+    }
+    @RequestMapping(value = "/aggiorna07")
+    public RegistrazioneDto07 aggiorna(){
+        RegistrazioneDto07 rx = new RegistrazioneDto07();
+        rx.setSessione("aggiorna");
         return rx;
     }
 }
