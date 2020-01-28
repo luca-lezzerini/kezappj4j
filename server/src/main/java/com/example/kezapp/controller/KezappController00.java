76bc1b72834196d5313c0044250065c2b8e12f6f
Luca Lezzerini <luca@lezzerini.it>
2020-01-28 13:00:43 +0100

    Primo controller

--- server/src/main/java/com/example/kezapp/controller/KezappController00.java
+++ server/src/main/java/com/example/kezapp/controller/KezappController00.java
@@ -1,5 +1,18 @@
 package com.example.kezapp.controller;
 
+import com.example.kezapp.model.RegistrazioneDto00;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class KezappController00 {
+
+    @RequestMapping(value = "/registrazione00")
+    public RegistrazioneDto00 registrazione() {
+        System.out.println("Siamo in registrazione!");
+        RegistrazioneDto00 rx = new RegistrazioneDto00();
+        rx.setSessione("123stella!");
+        return rx;
+    }
 
 }
