25be65934e747cd3a914f5ae2a22fd432e0fcc92
Matteo Saccone <saccone.matteo.io@gmail.com>
2020-01-28 11:53:51 +0100

    Creato metodi setter getter per chat12

--- server/src/main/java/com/example/kezapp/model/Chat12.java
+++ server/src/main/java/com/example/kezapp/model/Chat12.java
@@ -10,7 +10,33 @@ package com.example.kezapp.model;
  * @author utente
  */
 public class Chat12 {
-    private long id;
+    private Long id;
     private String nickname;
     private String session;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getSession() {
+        return session;
+    }
+
+    public void setSession(String session) {
+        this.session = session;
+    }
+    
+    
 }
