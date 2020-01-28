a548190d6ed7a24f6e976560d967fdc0bde8f750
Nicolas Bovolenta <nichi2000@gmail.com>
2020-01-28 14:15:19 +0100

    creato controller/KezappController01.java

--- server/src/main/java/com/example/kezapp/controller/KezappController01.java
+++ server/src/main/java/com/example/kezapp/controller/KezappController01.java
@@ -5,10 +5,19 @@
  */
 package com.example.kezapp.controller;
 
-/**
- *
- * @author utente
- */
+import com.example.kezapp.model.RegistrazioneDto01;
+import com.example.kezapp.model.RichiediRegistrazioneDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class KezappController01 {
-    
+    @RequestMapping(value = "/registrazione01")
+    public RegistrazioneDto01 registrazione(){
+        System.out.println("Siamo in registrazione");
+        RegistrazioneDto01 rx = new RegistrazioneDto01();
+        rx.setSessione("123Stella!");
+        return rx;
+    }
 }
