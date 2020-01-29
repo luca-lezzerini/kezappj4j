cb1a3978bda3d386fa567051aeeb8abe5b62b11c
MirkoCocchi <mirko.cocchi.26.12.2000@gmail.com>
2020-01-29 11:25:11 +0100

    Test per vedere se funziona commit da casa

--- server/src/main/java/com/example/kezapp/controller/KezappController04.java
+++ server/src/main/java/com/example/kezapp/controller/KezappController04.java
@@ -1,7 +1,7 @@
 package com.example.kezapp.controller;
 
 import com.example.kezapp.model.RegistrazioneDto04;
-//import com.example.kezapp.model.RichiediRegistrazioneDto04;
+import com.example.kezapp.model.RichiediRegistrazioneDto04;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class KezappController04 {
 
     @RequestMapping(value = "/registrazione04")
-    public RegistrazioneDto04 registrazione(/*RichiediRegistrazioneDto04 reg*/) {
+    public RegistrazioneDto04 registrazione(RichiediRegistrazioneDto04 reg) {
         System.out.println("Siamo in registrazione!");
         return new RegistrazioneDto04();
     }
