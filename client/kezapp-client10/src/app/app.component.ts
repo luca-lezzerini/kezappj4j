fa1676a42d53552ce8012bc229569021bbbf1772
Manuel Massa <manueldeho@gmail.com>
2020-02-10 15:53:39 +0100

    Aggiornamento server e client

--- client/kezapp-client10/src/app/app.component.ts
+++ client/kezapp-client10/src/app/app.component.ts
@@ -1,5 +1,13 @@
+import { InviaMessaggioDto } from './InviaMessaggioDto';
+import { RichiediMessaggiDto } from './RichiediMessaggiDto';
+import { RegistrazioneDto } from './RegistrazioneDto';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { logging } from 'selenium-webdriver';
+import { RichiediRegistrazioneDto } from './RichiediRegistrazioneDto';
+import { Observable } from 'rxjs';
+import { Messaggio } from './Messaggio';
+import { Chat } from './Chat';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +15,43 @@ import { logging } from 'selenium-webdriver';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  nickName: string;
+  nickname: string;
   messaggio: string;
-  righe: string[][] = [['Row1','Text','Text','Text'],['Row2','Text','Text','Text'],['Row3','Text','Text','Text'],['Row4','Text','Text','Text']];
   sessione: string;
-  mostraTesto: boolean = false;
+  messaggi: Messaggio[] = [];
+  contatti: Chat[]      = [];
 
-  nascondiTesto() {
-    this.mostraTesto=true
+  constructor( private http: HttpClient ) { }
+
+  registrazione(){
+    
+    //creo il dto con i dati da inviare
+    let dx: RichiediRegistrazioneDto = new RichiediRegistrazioneDto();
+    dx.nickname = this.nickname;
+
+    //preparare la richiesta http
+    let oss: Observable<RegistrazioneDto> = this.http.post<RegistrazioneDto>('http://localhost:8080/registrazione10', dx);
+
+    //creo la callback cioe definisco cosa deve fare quando ha la risposta pronta
+    oss.subscribe( risposta => {this.messaggi = risposta.messaggi; 
+                                this.contatti = risposta.contatti; 
+                                this.sessione = risposta.sessione;} )
   }
+
   inviaATutti(){
 
+    //preparo i dati da inviare al server
+    let im: InviaMessaggioDto = new InviaMessaggioDto();
+    im.destinatario = null;
+    im.sessione = this.sessione;
+    im.messaggio = this.messaggio;
+
+    //invio i dati al server
+    let ox: Observable<RegistrazioneDto> = this.http.post<RegistrazioneDto>('http://localhost:8080/inviatutti10', im);
+
+    //creo la callback
+    ox.subscribe( risposta => { this.messaggi=risposta.messaggi;
+                                this.contatti=risposta.contatti;} )
   }
+
 }
