9b12ef6f25771514be89e0b3661ba043802f80fd
Federica Dalla Valle <fede.dalla.valle@gmail.com>
2020-02-11 14:06:01 +0100

    Implementati services

--- client/kezapp-client06/src/app/app.component.ts
+++ client/kezapp-client06/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Messaggio } from './Messaggio';
 import { Chat } from './Chat';
+import { RichiediMessaggiDto } from './RichiediMessaggiDto';
 
 @Component({
   selector: 'app-root',
@@ -20,17 +21,19 @@ export class AppComponent {
   messaggi: Messaggio[] = [];
   contatti: Chat[] = [];
   sAttiva: boolean;
+
   constructor(private http: HttpClient) {}
 
   registrazione() {
     // creo il dto con i dati da inviare
-    let dx: RichiediRegistrazioneDto = new RichiediRegistrazioneDto();
+    const dx: RichiediRegistrazioneDto = new RichiediRegistrazioneDto();
     dx.nickname = this.nickname;
 
     // preparo la richiesta HTTP
-    let oss: Observable<RegistrazioneDto> =
-      this.http
-        .post<RegistrazioneDto>('http://localhost:8080/registrazione06', dx);
+    const oss: Observable<RegistrazioneDto> = this.http.post<RegistrazioneDto>(
+      'http://localhost:8080/registrazione06',
+      dx
+    );
 
     // creo la callback
     oss.subscribe(risposta => {
@@ -43,23 +46,63 @@ export class AppComponent {
   }
 
   inviaATutti() {
-        // preparo di dati da inviare al server
-        let im: InviaMessaggioDto = new InviaMessaggioDto();
-        im.messaggio = this.messaggio;
-        im.destinatario = null;
-        im.sessione = this.sessione;
+    // preparo di dati da inviare al server
+    const im: InviaMessaggioDto = new InviaMessaggioDto();
+    im.messaggio = this.messaggio;
+    im.destinatario = null;
+    im.sessione = this.sessione;
 
-        // invio i dati al server
-        let ox: Observable<RegistrazioneDto> =
-          this.http.post<RegistrazioneDto>('http://localhost:8080/invia-tutti06', im);
-        ox.subscribe(data => {
-          this.messaggi = data.messaggi;
-          this.contatti = data.contatti;
-        });
+    // invio i dati al server
+    const ox: Observable<RegistrazioneDto> = this.http.post<RegistrazioneDto>(
+      'http://localhost:8080/invia-tutti06',
+      im
+    );
+
+    // creo la callback
+    ox.subscribe(data => {
+      this.messaggi = data.messaggi;
+      this.contatti = data.contatti;
+    });
   }
 
-  invia() {}
-  aggiorna() {}
+  invia(contatto: Chat) {
+    // preparo di dati da inviare al server
+    const im: InviaMessaggioDto = new InviaMessaggioDto();
+    im.messaggio = this.messaggio;
+    im.destinatario = contatto.nickname;
+    im.sessione = this.sessione;
+
+    // invio i dati al server
+    const ox: Observable<RegistrazioneDto> = this.http.post<RegistrazioneDto>(
+      'http://localhost:8080/invia06',
+      im
+    );
+
+    // creo la callback
+    ox.subscribe(data => {
+      this.messaggi = data.messaggi;
+      this.contatti = data.contatti;
+    });
+  }
+
+  aggiorna() {
+    // preparo di dati da inviare al server
+    const rm: RichiediMessaggiDto = new RichiediMessaggiDto();
+    rm.sessione = this.sessione;
+
+    // invio i dati al server
+    const ox: Observable<RegistrazioneDto> = this.http.post<RegistrazioneDto>(
+      'http://localhost:8080/aggiorna06',
+      rm
+    );
+
+    // creo la callback
+    ox.subscribe(data => {
+      this.messaggi = data.messaggi;
+      this.contatti = data.contatti;
+    });
+  }
+
   sessioneAttiva() {
     if (this.sessione == null) {
       this.sAttiva = false;
